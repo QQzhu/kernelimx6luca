7d3e661b73d0e25ab36abe946f2c45cdf9fc86cd
QQzhu <qqzhu222888@163.com>
2020-09-07 18:53:15 +0800

    finish simplify luca dts and dtsi

--- build_qt_kernel.sh
+++ build_qt_kernel.sh
@@ -1,5 +1,2 @@
 #!/bin/sh
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- distclean
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- imx_luca_emmc_defconfig
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- menuconfig
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- all -j8
